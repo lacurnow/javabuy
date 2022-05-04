c367557c8965963a1befb2158cae776f8b15cab3
Eleanor Chamberlain-Stoltzfus <eleanor.stoltzfus@gmail.com>
2022-05-04 16:05:06 +0100

    mapping and join columns completed

---
+++ src/main/java/com/makersacademy/javabuy/model/Review.java
@@ -0,0 +1,61 @@
+package com.makersacademy.javabuy.model;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.GenerationType;
+import static java.lang.Boolean.TRUE;
+import static java.lang.Boolean.FALSE;
+
+import java.math.BigDecimal;
+
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "REVIEWS")
+
+public class Review {
+  
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+  private String title;
+  private String content;
+  private Integer rating;
+  private String photo;
+
+  @ManyToOne(fetch=FetchType.LAZY)
+  @JoinColumn(name="user_id")
+  private User user;
+
+  public Review() {}
+
+  public Review(String title, String content, Integer rating, String photo) {
+    this.title = title;
+    this.content = content;
+    this.rating = rating;
+    this.photo = photo;
+  }  
+  
+  public Long getId() { return this.id; }
+
+  public String getTitle() { return this.title; }
+  public void setTitle(String title) { this.title = title; }
+
+  public String getContent() { return this.content; }
+  public void setContent(String content) { this.content = content; }
+
+  public Integer getRating() { return this.rating; }
+  public void setRating(Integer rating) { this.rating = rating; }
+
+  public String getPhoto() { return this.photo; }
+  public void setPhoto(String photo) { this.photo = photo; }
+
+  public User getUser() { return this.user; }
+  public void setUser(User user) { this.user = user; }
+}

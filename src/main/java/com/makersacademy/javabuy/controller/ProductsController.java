10b6b5123759c76f365c0a00a814d78257592071
Leigh-Rose <leigh.a.rose@gmail.com>
2022-05-04 14:51:33 +0100

    filter now working

--- src/main/java/com/makersacademy/javabuy/controller/ProductsController.java
+++ src/main/java/com/makersacademy/javabuy/controller/ProductsController.java
@@ -2,6 +2,7 @@ package com.makersacademy.javabuy.controller;
 
 // import org.omg.CORBA.Request;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -9,6 +10,8 @@ import org.springframework.web.servlet.view.RedirectView;
 
 import java.security.Principal;
 import java.util.List;
+
+import com.makersacademy.javabuy.service.ProductsService;
 
 import com.makersacademy.javabuy.model.Product;
 import com.makersacademy.javabuy.model.User;
@@ -23,6 +26,9 @@ public class ProductsController {
 
     @Autowired
     UserRepository userRepository;
+
+    @Autowired
+    private ProductsService service;
 
     @GetMapping("/products")
     public String index(Model model) {
@@ -49,6 +55,16 @@ public class ProductsController {
     public String index() {
         return "payment/index";
     }
+
+    @GetMapping("/products/search")
+    public String viewSearch(Model model, @Param("keyword") String keyword) {
+        List<Product> listProducts = service.listAll(keyword);
+        model.addAttribute("listProducts", listProducts);
+        model.addAttribute("keyword", keyword);
+         
+        return "/products/search";
+    }
+     
 
 
 }
